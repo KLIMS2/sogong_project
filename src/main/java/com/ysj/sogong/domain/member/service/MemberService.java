4de9ecabe261046e7046c491cebf0fbe1e19b2e2
KLIMS2 <ysj069898@gmail.com>
2026-02-01 10:18:28 +0900

    회원가입 유효성 검사

--- src/main/java/com/ysj/sogong/domain/member/service/MemberService.java
+++ src/main/java/com/ysj/sogong/domain/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.ysj.sogong.domain.member.service;
 
 import com.ysj.sogong.domain.member.entity.Member;
+import com.ysj.sogong.domain.member.form.MemberForm;
 import com.ysj.sogong.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,13 +14,20 @@ public class MemberService
   private final MemberRepository memberRepository;
   private final PasswordEncoder passwordEncoder;
 
-  public Member createMember(Member memberDto)
+  public Member createMember(MemberForm memberForm)
   {
-    String encodedPassword = passwordEncoder.encode(memberDto.getPassword());
+    String encodedPassword = passwordEncoder.encode(memberForm.getPassword());
     Member member = Member.builder()
-        .username(memberDto.getUsername())
+        .username(memberForm.getUsername())
         .password(encodedPassword)
         .build();
+    return memberRepository.save(member);
+  }
+
+  public Member createMember(Member member)
+  {
+    String encodedPassword = passwordEncoder.encode(member.getPassword());
+    member.setPassword(encodedPassword);
     return memberRepository.save(member);
   }
 
